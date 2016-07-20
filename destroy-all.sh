8bb9d4c0f25083ed2dec5c0e7f1d47e457dff290
Slomic, Ismar <ismar@slomic.no>
2016-07-20 20:03:22 +0200

    adde vagrant global status at the end of bash script

--- destroy-all.sh
+++ destroy-all.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 for i in `vagrant global-status | grep virtualbox | awk '{ print $1 }'` ; do vagrant destroy $i ; done
 for i in `vagrant global-status | grep docker | awk '{ print $1 }'` ; do vagrant destroy $i ; done
+echo "Vagrant global status:" 
+vagrant global-status --prune
