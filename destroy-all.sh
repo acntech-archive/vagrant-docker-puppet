59ce999dc1985d6e395685154ba17f89c4b55144
Slomic, Ismar <ismar@slomic.no>
2016-07-20 19:54:54 +0200

    added bash script for deleting all docker and virtualbox vagrant machines

--- destroy-all.sh
+++ destroy-all.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
 for i in `vagrant global-status | grep virtualbox | awk '{ print $1 }'` ; do vagrant destroy $i ; done
+for i in `vagrant global-status | grep docker | awk '{ print $1 }'` ; do vagrant destroy $i ; done
